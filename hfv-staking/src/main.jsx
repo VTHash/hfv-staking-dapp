3f4d43ae871e86d348af68b27c92597870e7364d
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-01 22:36:53 +0000

    Configure WalletConnect for Ethereum Mainnet only

--- hfv-staking/src/main.jsx
+++ hfv-staking/src/main.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  Web3Modal,
+  WagmiConfig,
+  wagmiConfig,
+  ethereumClient,
+  projectId,
+} from "./walletConnectConfig";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={wagmiConfig}>
+      <App />
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </WagmiConfig>
   </React.StrictMode>
 );
-
-// Register service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then(
-      () => {
-        console.log("SW registered");
-      },
-      (err) => {
-        console.error("SW registration failed:", err);
-      }
-    );
-  });
-}
