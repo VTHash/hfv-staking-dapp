bb77627bb443150ea95744033b89ff9e195272ff
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-02 23:14:45 +0000

    fix: add Netlify Vite plug in and build support

--- hfv-staking/vite.config.js
+++ hfv-staking/vite.config.js
@@ -1,8 +1,10 @@
+import netlify from '@netlify/vite-plugin';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  plugins: [netlify()],
   build: {
     outDir: 'dist',
     rollupOptions: {
