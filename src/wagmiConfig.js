66f187adf33e92a055c182c74f1b003a0ea2e564
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-13 23:56:57 +0000

    Updated to wagmi@2 and viem@2 with clean Vite build

--- src/wagmiConfig.js
+++ src/wagmiConfig.js
@@ -1,8 +1,9 @@
-import { configureChains, createConfig, WagmiProvider } from 'wagmi';
+import { createConfig, WagmiProvider} from 'wagmi'
+import { configureChains } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 import { http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { EthereumClient } from '@web3modal/ethereum';
+import { EthereumClient } from '@web3modal/wagmi';
 import { Web3Modal } from '@web3modal/react';
 import { walletConnect } from '@web3modal/wagmi'
 const projectId = import.meta.env.VITE_PROJECT_ID;
