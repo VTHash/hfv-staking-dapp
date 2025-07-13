792031bc8f0b79a3dae0c14b115615c4453916cc
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-13 23:25:39 +0000

    Upgrade to Web3modal v3 with Reown AppKit support and wagmi v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App';
 import { wagmiConfig } from './wagmiConfig';
