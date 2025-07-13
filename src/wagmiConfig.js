7bb16844cd5d07c5b0429d154b52982d79ffaee5
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-13 23:20:50 +0000

    Fix walletConnect import path and upgrade to latest wagmi/web3modal

--- src/wagmiConfig.js
+++ src/wagmiConfig.js
@@ -4,7 +4,7 @@ import { http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { EthereumClient } from '@web3modal/ethereum';
 import { Web3Modal } from '@web3modal/react';
-
+import { walletConnect } from '@web3modal/wagmi'
 const projectId = import.meta.env.VITE_PROJECT_ID;
 
 const { chains, publicClient } = configureChains(
