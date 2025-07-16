53be09bdc568331eebe51f40541c8a4cca212d55
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-16 22:05:27 +0000

    Clean wagmi build hooks error for Netlify

--- src/components/NetworkBanner.jsx
+++ src/components/NetworkBanner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNetwork } from 'wagmi/hooks';
+import { useNetwork } from 'wagmi';
 
 const NetworkBanner = () => {
   const { chain, isConnected } = useNetwork();
