f6d4cc78051122eb333c8e638b50ae7feef2f0b3
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-17 17:02:48 +0000

    Fix vite config and Netlify build settings

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
+    react(),
     viteStaticCopy({
       targets: [
         {
           src: 'public/_redirects',
-          dest: '.' 
+          dest: '.'
         }
       ]
     })
