050d9954b76a613d325d02ea4db452be9c5d6ce8
HighFinanceVentures <hfv@highfinanceventures.com>
2025-07-17 18:50:45 +0000

    Fix: Appkit config + cleanup Web3modal

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import {
-  Web3Modal,
-  EthereumClient
-} from "@web3modal/ethereum";
-import { Web3Modal as Modal } from "@web3modal/react";
-import { wagmiConfig, ethereumClient, projectId } from "./walletConnectConfig";
-
-import { WagmiConfig } from "wagmi";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <App />
-      <Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </WagmiConfig>
+    <App />
   </React.StrictMode>
 );
