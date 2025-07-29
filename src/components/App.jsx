dc05bc18381cb4c8b3db2f1f1a017da56aff79c1
HFVprotocol <andreiraduhabliuc@gmail.com>
2025-07-29 03:45:46 +0000

    Fixed App for mobile view UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,35 @@
-import React from 'react';
-import ConnectWallet from './ConnectWallet';
-import StakeForm from './StakeForm';
-import StakingDashboard from './StakingDashboard';
-import RewardCalculator from './RewardCalculator';
-import NetworkBanner from './NetworkBanner';
-import WalletStatus from './WalletStatus';
-import Claim from './ClaimHFV';
-import '../index.css';
+import React from 'react'; import ConnectWallet from './ConnectWallet'; import StakeForm from './StakeForm'; import StakingDashboard from './StakingDashboard'; import RewardCalculator from './RewardCalculator'; import NetworkBanner from './NetworkBanner'; import WalletStatus from './WalletStatus'; import Claim from './ClaimHFV'; import '../index.css';
 
-const App = () => {
-  return (
-    <div className="app-wrapper">
-      <div className="app-header">
-        <img src="/hfv-logo.png" alt="HFV Logo" className="logo" />
-        <h1 className="app-title">HFV Staking DApp</h1>
-      </div>
+const App = () => { return ( <div className="app-wrapper"> <div className="app-header"> <img src="/hfv-logo.png" alt="HFV Logo" className="logo" /> <h1 className="app-title">HFV Staking DApp</h1> </div>
 
-      <div className="main-content">
-        <div className="glow-frame">
-          <ConnectWallet />
-          <NetworkBanner />
-        </div>
-
-        <div className="glow-frame">
-          <StakeForm />
-        </div>
-
-        <div className="glow-frame">
-          <StakingDashboard />
-        </div>
-
-        <div className="glow-frame">
-          <Claim />
-        </div>
-
-        <div className="glow-frame">
-          <RewardCalculator />
-        </div>
-
-        <div className="glow-frame">
-          <WalletStatus />
-        </div>
-      </div>
+<div className="main-content">
+    <div className="glow-frame">
+      <ConnectWallet />
+      <NetworkBanner />
     </div>
-  );
-};
+
+    <div className="glow-frame">
+      <StakeForm />
+    </div>
+
+    <div className="glow-frame">
+      <StakingDashboard />
+    </div>
+
+    <div className="glow-frame">
+      <Claim />
+    </div>
+
+    <div className="glow-frame">
+      <RewardCalculator />
+    </div>
+
+    <div className="glow-frame">
+      <WalletStatus />
+    </div>
+  </div>
+</div>
+
+); };
 
 export default App;
