2f52576eb2b776f64bb7e2454e2e3cc528998f79
HFVprotocol <andreiraduhabliuc@gmail.com>
2025-08-08 18:31:50 +0000

    Fix wallet not triggering before stake forcing eth_requestAccounts and enable()

--- src/components/StakeForm.jsx
+++ src/components/StakeForm.jsx
@@ -3,7 +3,7 @@ import { ethers, BrowserProvider } from 'ethers';
 import EthereumProvider from '@walletconnect/ethereum-provider';
 import HFVStaking from '../abi/HFVStaking.json';
 import HFVToken from '../abi/HFVToken.json';
-import { Interface } from 'ethers';
+
 const stakingAbi = HFVStaking.abi;
 const tokenAbi = HFVToken.abi;
 
